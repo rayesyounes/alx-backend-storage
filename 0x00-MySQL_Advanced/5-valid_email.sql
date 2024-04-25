59083fb0145064dcbbfebc24ecfd95e78006ee9d
rayesyounes <rayesyounes88@gmail.com>
2024-04-25 22:00:17 +0000

    Email validation to sent

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,9 @@
+-- SQL script that creates a trigger that resets valid_email only when the email has been changed.
+
+CREATE TRIGGER email_reset AFTER UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+        UPDATE users SET NEW.valid_email = 0
+    END IF;
+END;
