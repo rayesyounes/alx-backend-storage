ffd53689194b9ba112556243277df6df09c0697e
rayesyounes <rayesyounes88@gmail.com>
2024-04-29 08:53:50 -0700

    fix ?

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,7 +2,7 @@
 """ A script that that lists all documents in a collection"""
 
 def list_all(mongo_collection):
-
+    """ A script that that lists all documents in a collection"""
     docs = []
     for doc in mongo_collection.find():
         if doc:
