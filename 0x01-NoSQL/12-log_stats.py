4ba9a6dceebf226d5e62677b6b13194aa49bc275
rayesyounes <rayesyounes88@gmail.com>
2024-04-30 16:23:16 -0700

    Log stats

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""a Python script that provides some stats about Nginx logs stored in MongoDB"""
+
+
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    coll = client.logs.nginx
+
+    all_logs = coll.count_documents({})
+    get_logs = coll.count_documents({"method": "GET"})
+    post_logs = coll.count_documents({"method": "POST"})
+    put_logs = coll.count_documents({"method": "PUT"})
+    patch_logs = coll.count_documents({"method": "PATCH"})
+    delete_logs = coll.count_documents({"method": "DELETE"})
+    checks = coll.count_documents({"method": "GET", "path": "/status"})
+
+    print("{} logs\n Methods:\n \t  method GET: {} \n\t  method POST: {} \n\t  method"
+              " PUT: {} \n\t  method PATCH: {} \n\t  method DELETE: {}"
+              "\n status_checks: {}".format(all_logs, get_logs, post_logs, put_logs, patch_logs, delete_logs, checks))
